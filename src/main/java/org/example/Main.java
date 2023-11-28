ad90f558d6767d8dfe1d77b80efb602aaf3e8279
Luis Camilo Rosania Rivera <bamira1@misena.edu.co>
2023-11-28 11:30:25 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Natillera la abuelita toretto
+        //¿Qué necesito para procesar la natillera de 1 cliente?
+        //ENTRADAS
+        int identificadorCliente=233444;
+        long documentoIdentidad=100223456754L;
+        byte edadCliente=34;
+        short cuotaMensualCliente=25000;
+        float tasaInteresFijo=0.1f;
+        char calificacionCliente='A';
+
+
+    }
+}
